e199b43a8e64de81ce3be3a3ce011bd34726a50d
Noah Nguyen <noahtngu@gmail.com>
2023-04-01 20:50:09 -0500

    removed array

--- src/components/MedicalHistoryForm.tsx
+++ src/components/MedicalHistoryForm.tsx
@@ -1,6 +1,7 @@
 import { type ChangeEvent, useState } from "react";
+import { number } from "zod";
 
-const pmh = [
+/*const pmh = [
   "ADHD",
   "Alcoholism",
   "Allergies",
@@ -49,24 +50,35 @@ const pmh = [
   "Stroke",
   "Thyroid Disorder",
   "Ulcerative Colitis",
-];
+];*/
 
 const Create = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [singleMed, setSingleMed] = useState("")
   const [personalMedHist, setPersonalMedHist] = useState<string[]>([]);
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    const { value, checked } = event.target;
-
-    if (checked) {
-      setPersonalMedHist((pre) => [...pre, value]);
-    } else {
-      setPersonalMedHist((pre) => {
-        return [...pre.filter((skill) => skill != value)];
-      });
-    }
+  const addField = () =>{
+    setPersonalMedHist([...personalMedHist, singleMed])
+    setSingleMed("")
   }
+  const removeField = (index: number) =>{
+    const list = [...personalMedHist]
+    list.splice(index,1);
+    setPersonalMedHist(list);
+  }
+
+//   function handleChange(event: ChangeEvent<HTMLInputElement>) {
+//     const { value, checked } = event.target;
+
+//     if (checked) {
+//       setPersonalMedHist((pre) => [...pre, value]);
+//     } else {
+//       setPersonalMedHist((pre) => {
+//         return [...pre.filter((skill) => skill != value)];
+//       });
+//     }
+//   }
 
   return (
     <div className="create">
@@ -106,20 +118,26 @@ const Create = () => {
         </div>
         <div className="mb-4">
           <label className="mb-2 block font-bold text-gray-700">
-            Personal Medical History:
+            Personal Medical History (Any surgeries, complications, diseases):
           </label>
-          {pmh.map((pmh, index) => (
-            <div key={index} className="flex items-center">
-              <input
-                className="mr-2 leading-tight"
-                type="checkbox"
-                required
-                value={pmh}
-                onChange={handleChange}
-              />
-              <span className="text-gray-700">{pmh}</span>
-            </div>
-          ))}
+          <input
+                    className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 focus:outline-none"
+                    type="text"
+                    id="singleMed"
+                    value={singleMed}
+                    onChange={(e) => setSingleMed(e.target.value)} />
+                    <button type="button" onClick ={addField}>Add</button>
+
+          {personalMedHist.map((x,i) => {
+            return(
+                <div className = "flex">
+                    <p>{x}</p>
+                    <button onClick={() => removeField(i)}>Remove</button>
+                </div>
+          )
+          })}
+
+
         </div>
         <button
           className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
